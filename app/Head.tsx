dc70c6d8a89d17e1d8138b0d9c70788e8b295079
hieuph <hieuph.dev@gmail.com>
2023-11-07 09:36:35 +0700

    update sth

--- app/Head.tsx
+++ app/Head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
     return (
-      <>
-        <title>NEX's Blog</title>
+      <div>
+        <title>NEX Blog</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
-      </>
+      </div>
     );
   }
