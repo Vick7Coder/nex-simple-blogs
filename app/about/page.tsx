6a5e899d632ef0c96e9a9d349a2cf86568c7ad82
hieuph <hieuph.dev@gmail.com>
2023-11-09 12:29:52 +0700

    update sth

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const AboutUs = () => {
     return (
-        <section className="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
+        <div className="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
         <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
             <div className="flex flex-wrap ">
                 <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
@@ -46,7 +46,7 @@ const AboutUs = () => {
                 </div>
             </div>
         </div>
-    </section>
+    </div>
     );
 };
 export default AboutUs;
